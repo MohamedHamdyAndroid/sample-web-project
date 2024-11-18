6cbe825115b3c67cfdcd2324416dbf79679f7c83
Mohamed Hamdy <mohamed.hamdy.dev@gmail.com>
2024-11-18 20:53:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Mohamed Hamdy');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
